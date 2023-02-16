017df77d24921242c6511350d2aa0143da53b473
IvanMkrv <makarovrabota@mail.ru>
2023-02-16 21:23:54 +0300

    Первое задание с условием если

---
+++ praktika5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите Ваше имя:");
+string username = Console.ReadLine();
+if (username == "Маша")
+{
+Console.WriteLine("Огооогооооо это Маша!!!!!!");
+}
+else 
+{
+    Console.Write ("Привет, дорогой!!"); 
+    Console.WriteLine (username);
+};
+
+
