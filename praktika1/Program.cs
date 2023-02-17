0ec2f83cfec0bb73c4519fc5b7c30a0b8d766aee
IvanMkrv <123653934+IvanMkrv@users.noreply.github.com>
2023-02-17 20:30:41 +0300

    Update Program.cs
    Задача на определение четвертей добавлена

--- praktika1/Program.cs
+++ praktika1/Program.cs
@@ -1,3 +1,27 @@
 ﻿Console.WriteLine(" Напишите имя");
 Console.WriteLine(" 2 Hello, World!");
 Console.WriteLine(" 2 Hello, World!");
+
+
+
+//Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+Console.WriteLine("Введите X:");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y:");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.WriteLine("1 четверть");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2 четверть");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3 четверть");
+else if (x > 0 && y < 0)
+    Console.WriteLine("4 четверть");
+else
+    Console.WriteLine("четверть не определена");
