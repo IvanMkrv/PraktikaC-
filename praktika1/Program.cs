ff658ca79ecd25c3e163de3b61d572fd191692f9
IvanMkrv <123653934+IvanMkrv@users.noreply.github.com>
2023-02-17 20:52:22 +0300

    Update Program.cs
    решение через свитч

--- praktika1/Program.cs
+++ praktika1/Program.cs
@@ -51,3 +51,22 @@ else if (num == 4)
 else
   Console.WriteLine("четверть не существует");
 
+// та же задача решение через свитч типа кейсы
+
+Console.Clear();
+
+Console.WriteLine("Введем номер четверти:");
+int Chetv = int.Parse(Console.ReadLine());
+switch (Chetv)
+{
+    case 1:
+        Console.WriteLine("x>0 & y>0");
+        break;
+    case 2:
+        Console.WriteLine("x<0 & y>0");
+        break;
+    case 3:
+        Console.WriteLine("x<0 & y<0");
+        break;
+    case 4:
+        Console.WriteLine("x>0 & y<0");
