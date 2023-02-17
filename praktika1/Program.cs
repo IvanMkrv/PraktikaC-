648fde81497e0a91f2f58ad0c826d5344b3690d7
IvanMkrv <123653934+IvanMkrv@users.noreply.github.com>
2023-02-17 21:53:34 +0300

    Update Program.cs
    Третья задача

--- praktika1/Program.cs
+++ praktika1/Program.cs
@@ -33,6 +33,26 @@ Console.WriteLine("Введите номер четверти");
 int num = int.Parse(Console.ReadLine());
 
 if (num == 1)
+    
+ // Решение задачи в семинаре 3 введение в квардрат числа
+    
+    void PrintSqr(int num)
+{
+ int i = 1;
+ while (i<num)
+ {
+    
+    Console.Write($"{Math.Pow(i,2)}, ");
+    i++;
+ }    
+    Console.Write($"{Math.Pow(i,2)}");
+}
+
+
+Console.Write("введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+PrintSqr(num);
+    
 {
   Console.WriteLine("x > 0; y > 0");
 }
